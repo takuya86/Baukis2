7fa3cf2e18c390765563167a6058a72caca3fdf4
takuya86 <takuya86takuya@gmail.com>
2021-02-23 15:47:16 +0000

    Chapter6 6-2-3完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  #StandardErrorがあったら、rescue500メソッドを呼び出す
+  rescue_from StandardError, with: :rescue500
+
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       "customer"
     end
+  end
+
+  #StandardErrorがあったときに表示されるページの設定
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
   end
 end
